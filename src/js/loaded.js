a059b8b48a5d7247dcc9e10377168e447e652808
tetsuwo <tetsuwo.oishi@gmail.com>
2012-08-08 18:21:46 +0900

    modified scripts

--- src/js/loaded.js
+++ src/js/loaded.js
@@ -4,10 +4,28 @@
     $paper.width(window.innerWidth / 2);
     $paper.height(window.innerHeight);
 
-
-
-
-
     $('body').show();
+
+    $('.load').click(function() {
+        var name = window.prompt('Input you are want to load the file name');
+        console.log(name);
+
+        var papers = JSON.parse(localStorage.getItem('papers'));
+        var text = JSON.parse(papers[name]);
+        $paper.val(text);
+    });
+
+    $('.save').click(function() {
+        var name = window.prompt('What is file name?');
+        console.log(name);
+
+        var papers = JSON.parse(localStorage.getItem('papers')) || {};
+        papers[name] = JSON.stringify($paper.val());
+        localStorage.setItem('papers', JSON.stringify(papers));
+    });
+
+    $(window).bind('beforeunload', function() {
+        return 'データ消えちゃうよ！';
+    });
 
 })(jQuery);
